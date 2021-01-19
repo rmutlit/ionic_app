978e5252d82a1c776c168a84efe9fff5c7f82560
patcharaphon manom <patcharaphon.mn@gmail.com>
2021-01-19 17:17:09 +0700

    ครั้งที่ 1

---
+++ src/views/Product.vue
@@ -0,0 +1,38 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>สินค้า</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">สินค้า</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      
+  
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import axios from 'axios';
+
+
+export default  {
+  name: "product",
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage },
+  setup() {
+    return {
+      products: null,
+    };
+  },
+  async mounted() {
+     const data = await axios.get("http://localhost:3000/products");
+     console.log(data.data);  
+  }
+};
+</script>
